6a09d9f4ee6a6d3a659a829f9966faaf4bd9ea76
macros0 <20460562+macros0@users.noreply.github.com>
2019-02-11 10:11:46 +0300

    Update NooLite.py in fan
    Missing CONF_SCAN_INTERVAL parameter in PLATFORM_SCHEMA
    HASIO version 0.87.0 does not update the status

--- custom_components/fan/NooLite.py
+++ custom_components/fan/NooLite.py
@@ -5,12 +5,13 @@ For more details about this platform, please refer to the documentation
 https://home-assistant.io/components/demo/
 """
 import logging
+from datetime import timedelta
 
 import voluptuous as vol
 from homeassistant.components.fan import (FanEntity, SUPPORT_SET_SPEED,
                                           SUPPORT_OSCILLATE, SUPPORT_DIRECTION)
 from homeassistant.const import CONF_NAME, CONF_MODE
-from homeassistant.const import CONF_TYPE
+from homeassistant.const import CONF_TYPE, CONF_SCAN_INTERVAL
 from homeassistant.helpers import config_validation as cv
 
 from custom_components.NooLite import CONF_BROADCAST, CONF_CHANNEL
@@ -24,11 +25,14 @@ DEPENDENCIES = ['NooLite']
 
 TYPES = ['Fan']
 
+SCAN_INTERVAL = timedelta(seconds=60)
+
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_TYPE, default='Fan'): vol.In(TYPES),
     vol.Optional(CONF_BROADCAST, default=False): cv.boolean,
+    vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL): cv.time_period,
     vol.Required(CONF_NAME): cv.string,
     vol.Required(CONF_CHANNEL): cv.positive_int,
     vol.Required(CONF_MODE): cv.string,
