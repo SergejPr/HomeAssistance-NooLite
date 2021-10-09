8266cf9a6edfcb150d184e7ca44d55e94da0c343
Sergey Prytkov <sprytkov@qulix.com>
2021-10-10 01:11:19 +0300

    update fan module

--- custom_components/noolite/fan.py
+++ custom_components/noolite/fan.py
@@ -2,8 +2,7 @@ import logging
 from datetime import timedelta
 
 import voluptuous as vol
-from homeassistant.components.fan import (FanEntity, SUPPORT_SET_SPEED, SUPPORT_OSCILLATE, SUPPORT_DIRECTION,
-                                          SPEED_LOW, SPEED_MEDIUM, SPEED_HIGH, SPEED_OFF)
+from homeassistant.components.fan import (FanEntity, SUPPORT_SET_SPEED, SUPPORT_PRESET_MODE)
 from homeassistant.const import CONF_NAME, CONF_MODE, CONF_SCAN_INTERVAL
 from homeassistant.helpers import config_validation as cv
 
@@ -11,18 +10,19 @@ from custom_components.noolite import (CONF_BROADCAST, CONF_CHANNEL, MODES_NOOLI
                                        NooLiteGenericModule, DOMAIN)
 from custom_components.noolite import (PLATFORM_SCHEMA)
 
-FULL_SUPPORT = SUPPORT_SET_SPEED | SUPPORT_OSCILLATE | SUPPORT_DIRECTION
-LIMITED_SUPPORT = SUPPORT_SET_SPEED
-
 DEPENDENCIES = ['noolite']
+
+CONF_SPEED_ENABLED = "speed_enabled"
 
 _LOGGER = logging.getLogger(__name__)
 
 _SCAN_INTERVAL = timedelta(seconds=60)
+_DEFAULT_SPEED = 50
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_BROADCAST, default=False): cv.boolean,
     vol.Optional(CONF_SCAN_INTERVAL, default=_SCAN_INTERVAL): cv.time_period,
+    vol.Optional(CONF_SPEED_ENABLED, default=False): cv.boolean,
     vol.Required(CONF_NAME): cv.string,
     vol.Required(CONF_CHANNEL): cv.positive_int,
     vol.Required(CONF_MODE, default=MODE_NOOLITE_F): vol.In(MODES_NOOLITE),
@@ -38,7 +38,8 @@ class NooLiteFan(NooLiteGenericModule, FanEntity):
 
     def __init__(self, config, device):
         super().__init__(config, device)
-        self._speed = SPEED_OFF
+        self._speed_mode = config[CONF_SPEED_ENABLED]
+        self._percentage = _DEFAULT_SPEED
 
     @property
     def is_on(self):
@@ -46,66 +47,44 @@ class NooLiteFan(NooLiteGenericModule, FanEntity):
         return self._state
 
     @property
-    def speed(self) -> str:
-        """Return the current speed."""
-        return self._speed
-
-    @property
-    def speed_list(self) -> list:
-        """Get the list of available speeds."""
-        return [SPEED_OFF, SPEED_LOW, SPEED_MEDIUM, SPEED_HIGH]
-
-    @property
     def supported_features(self) -> int:
         """Flag supported features."""
-        # TODO: Check speed feature. Seems for it should be returned 1
-        return 0
+        if self._speed_mode is True:
+            return SUPPORT_SET_SPEED | SUPPORT_PRESET_MODE
+        else:
+            return 0
 
-    def turn_on(self, speed: str = None, **kwargs) -> None:
+    def turn_on(self, percentage: int, **kwargs) -> None:
         """Turn on the entity."""
-        if speed is None:
-            speed = SPEED_MEDIUM
-        self.set_speed(speed)
+        if percentage is None:
+            percentage = self._percentage
+        self.set_percentage(percentage)
 
     def turn_off(self, **kwargs) -> None:
         """Turn off the entity."""
-        self.set_speed(SPEED_OFF)
+        self.set_percentage(0)
 
-    def set_speed(self, speed: str) -> None:
+    def set_percentage(self, percentage: int) -> None:
         """Set the speed of the fan."""
-        if speed is None:
-            speed = self._speed
+        if percentage is None:
+            percentage = self._percentage
 
-        if speed == SPEED_HIGH:
-            int_speed = 255
-        elif speed == SPEED_MEDIUM:
-            int_speed = 180
-        elif speed == SPEED_LOW:
-            int_speed = 70
-        else:
-            int_speed = 0
-
-        responses = self._device.set_brightness(int_speed / 255, None, self._channel, self._broadcast, self._mode)
+        responses = self._device.set_brightness(percentage / 100, None, self._channel, self._broadcast, self._mode)
         if self.assumed_state:
-            self._state = speed != SPEED_OFF
-            self._speed = speed
+            self._state = percentage != 0
+            self._percentage = percentage
         else:
             self._update_state_from(responses)
 
         self.schedule_update_ha_state()
+
+    @property
+    def percentage(self) -> int:
+        return self._percentage
 
     def set_direction(self, direction: str) -> None:
         pass
 
     def _update_state_from(self, responses):
         super()._update_state_from(responses)
-        int_speed = int(self._level * 255)
-
-        if 0 < int_speed <= 80:
-            self._speed = SPEED_LOW
-        elif 80 < int_speed <= 180:
-            self._speed = SPEED_MEDIUM
-        elif 180 < int_speed:
-            self._speed = SPEED_HIGH
-        else:
-            self._speed = SPEED_OFF
+        self._percentage = self._level * 100
